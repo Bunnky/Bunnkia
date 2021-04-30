7025b3399cd9d850be11f77a122cb8f5c389a63b
Bill <bunnkywhat@gmail.com>
2021-04-30 08:38:26 -0400

    pre-97

--- SFML-Bunnkia/PlayerGUI.cpp
+++ SFML-Bunnkia/PlayerGUI.cpp
@@ -1,22 +1,58 @@
 #include "stdafx.h"
 #include "PlayerGUI.h"
 
+void PlayerGUI::initFont()
+{
+	this->font.loadFromFile("Fonts/The Impostor.ttf");
+}
+
+void PlayerGUI::initHPBar()
+{
+	float width = 150.f;
+	float height = 20.f;
+	float x = 20.f;
+	float y = 20.f;
+
+	this->hpBarBack.setSize(sf::Vector2f(width, height));
+	this->hpBarBack.setFillColor(sf::Color(50, 50, 50, 200));
+	this->hpBarBack.setPosition(x, y);
+
+	this->hpBarInner.setSize(sf::Vector2f(width, height));
+	this->hpBarInner.setFillColor(sf::Color(250, 20, 20, 200));
+	this->hpBarInner.setPosition(this->hpBarBack.getPosition());
+}
+
 PlayerGUI::PlayerGUI(Player* player)
 {
 	this->player = player;
+
+	this->initFont();
+	this->initHPBar();
 }
 
 PlayerGUI::~PlayerGUI()
 {
+
 }
 
 //Functions
+void PlayerGUI::updateHPBar()
+{
+
+}
+
 void PlayerGUI::update(const float& dt)
 {
 
 }
 
+void PlayerGUI::renderHPBar(sf::RenderTarget& target)
+{
+	target.draw(this->hpBarBack);
+	target.draw(this->hpBarInner);
+}
+
 void PlayerGUI::render(sf::RenderTarget& target)
 {
-
+	this->renderHPBar(target);
 }
--- SFML-Bunnkia/PlayerGUI.h
+++ SFML-Bunnkia/PlayerGUI.h
@@ -9,12 +9,23 @@ class PlayerGUI
 private:
 	Player* player;
 
+	sf::Font font;
+	sf::RectangleShape hpBarBack;
+	sf::RectangleShape hpBarInner;
+
+
+	void initFont();
+	void initHPBar();
+
 public:
 	PlayerGUI(Player* player);
 	virtual ~PlayerGUI();
 
 	//Functions
+	void updateHPBar();
 	void update(const float& dt);
+
+	void renderHPBar(sf::RenderTarget& target);
 	void render(sf::RenderTarget& target);
 };
 
