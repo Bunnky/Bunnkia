063454d8bb1d4299d0b248ee52738ca2913153ba
Bill <bunnkywhat@gmail.com>
2022-05-31 16:44:19 -0400

    moved

--- SFML-Bunnkia/TileMap.cpp
+++ SFML-Bunnkia/TileMap.cpp
@@ -574,19 +574,15 @@ void TileMap::updateTiles(Entity* entity, const float& dt, EnemySystem& enemySys
 							{
 							case 0:
 								enemySystem.createEnemy(GOBLIN, x * this->gridSizeF, y * this->gridSizeF, *es);
-								std::cout << "Spawned Goblin" << "\n";
 								break;
 							case 1:
 								enemySystem.createEnemy(SKELETON, x * this->gridSizeF, y * this->gridSizeF, *es);
-								std::cout << "Spawned Skeleton" << "\n";
 								break;
 							case 2:
 								enemySystem.createEnemy(SLIME, x * this->gridSizeF, y * this->gridSizeF, *es);
-								std::cout << "Spawned Slime" << "\n";
 								break;
 							case 3:
 								enemySystem.createEnemy(SPIDER, x * this->gridSizeF, y * this->gridSizeF, *es);
-								std::cout << "Spawned Spider" << "\n";
 								break;
 							default:
 								std::cout << "Spawned NULL" << "\n";
